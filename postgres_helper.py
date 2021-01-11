5357603d4c7fa7dc154dc15c976202a4dea1b412
artmaguire <a.elnino11@gmail.com>
2021-01-11 16:08:04 +0000

    Cleanup of Python Code
    Signed-off-by: artmaguire <a.elnino11@gmail.com>

--- postgres_helper.py
+++ postgres_helper.py
@@ -14,7 +14,9 @@ def close_connection():
 
 def get_location_name(name):
     name += '%'
-    cur.execute("SELECT osm_id, name, ST_Y(st_transform(geometry, 4674)), ST_X(st_transform(geometry, 4674)) FROM osm_places WHERE name ILIKE %s ORDER BY name LIMIT 10", (name,))
+    cur.execute(
+            "SELECT osm_id, name, ST_Y(st_transform(geometry, 4674)), ST_X(st_transform(geometry, 4674)) FROM osm_places WHERE name ILIKE %s ORDER BY name LIMIT 10",
+            (name,))
     result = cur.fetchall()
     print(result)
     list_result = [{'osm_id': t[0], 'name': t[1], 'lat': t[2], 'lon': t[3]} for t in result]
