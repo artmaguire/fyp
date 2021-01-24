b5786809d04cde27974b9c50ba4fe9547878793e
artmaguire <a.elnino11@gmail.com>
2021-01-24 19:17:46 +0000

    Added template for loading screen Selection of transport icons Added to vue components
    Signed-off-by: artmaguire <a.elnino11@gmail.com>

---
+++ static/js/MainComponent.js
@@ -0,0 +1,35 @@
+const main = new Vue({
+    el: '#main-component',
+    data: {
+        additionalNodes: [],
+        isSplashLoading: true,
+        isLoading: false,
+        searchType: 'driving'
+    },
+    methods: {
+        startLoading(type) {
+            console.log(type);
+            this.searchType = type;
+            this.isLoading = true;
+            setTimeout(() => {
+                this.isLoading = false
+            }, 1000);
+        }
+    },
+    mounted() {
+        setTimeout(() => {
+            this.isSplashLoading = false;
+        }, 1000)
+    },
+    template: `
+    <div>
+        <splash-screen :active="isSplashLoading"></splash-screen>
+        <div v-bind:class="{ 'is-invisible': isSplashLoading }">
+    
+            <loading-screen v-bind:type="searchType" v-if="isLoading"></loading-screen>
+    
+            <div id="mapid"></div>
+            <search @loading="startLoading"></search>
+        </div>
+    </div>`
+});
